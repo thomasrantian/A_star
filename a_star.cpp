8507e809e20f84ddc2883b268a79b8f381eb4204
thomasrantian <tianran@umich.edu>
2018-09-28 15:39:20 -0700

    set up the grid class

--- a_star.cpp
+++ a_star.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 using namespace std;
+ #include <vector>
 
-int main() 
+
+
+// occupency grid class contsins the 
+class OccupancyGrid {
+public:
+	class node{
+	public:
+		int x; // index in the grid
+		int y;
+
+		int occupied_status; // occupied status;
+		char symble; // . if the node is empty; # if the node is occupied
+
+		double h; // heuristic cost
+		double g; // moving cost
+		
+		// constructor, assign x, y, set h, g to be zero by default
+		node(int x_in, int y_in) {
+
+		}
+
+	};
+
+	int length;
+	int width;
+	vector< vector<node> > grid;
+	// constructor: assign the length and width of the grid
+	OccupancyGrid(int l, int w) {
+
+	}
+
+	// randomly generating the grid
+	void generate_grid() {
+
+	}
+
+	// print the grid using the symble of the node : eg., ...###...
+	void print_grid() {
+
+	}
+
+};
+
+int main(int argc, char const *argv[])
 {
-    cout << "Hello, World!";
-    return 0;
+	//OccupancyGrid my_map(5,5);
+	//my_map.generate_grid();
+	//my_map.print_grid();
+	return 0;
 }
